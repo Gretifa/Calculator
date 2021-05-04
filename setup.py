b887cd2daff33406aa0154da218fbaae7255c20f
Gretifa <greta.budrauskaite@gmail.com>
2021-05-05 00:01:51 +0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="calculator",
+    version="0.0.1",
+    author="Greta",
+    description="Calculator",
+    url="https://github.com/Gretifa/Calculator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+    ]
+)
